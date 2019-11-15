e6a7e53c0f8ad0c13295c9c84fb1dadf80c1b93e
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2019-11-15 11:23:25 +1100

    Updates

---
+++ precache-manifest.c26f829f77e915df2f51ae79104237c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b787490870d155e2cf759540b5109d",
+    "url": "/basic-todo-list/index.html"
+  },
+  {
+    "revision": "7eb03c9f5fa2edbb1649",
+    "url": "/basic-todo-list/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f4b1ce5dfe40ef021752",
+    "url": "/basic-todo-list/static/js/2.0481ef67.chunk.js"
+  },
+  {
+    "revision": "7eb03c9f5fa2edbb1649",
+    "url": "/basic-todo-list/static/js/main.1e5f6048.chunk.js"
+  },
+  {
+    "revision": "84fb546ebf7a17c590a7",
+    "url": "/basic-todo-list/static/js/runtime-main.edc7793f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/basic-todo-list/static/media/logo.25bf045c.svg"
+  }
+]);
