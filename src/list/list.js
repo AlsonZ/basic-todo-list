85581ad0732728f2e8bdbcd356de8fd4fac7a78c
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2019-11-20 20:58:37 +1100

    add list element component

--- src/list/list.js
+++ src/list/list.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import ListElement from './listelement';
 import './style.css';
 
 function List() {
 
   const [elements, setElements] = useState([]);
-  const [action, setAction] = useState("+");
+  const [action, setAction] = useState("➕");
   const [box, setBox] = useState(true);
 
   useEffect(() => {
@@ -14,15 +15,19 @@ function List() {
   const checkSavedLists = () => {
     // check cookies or w/e to determine if there is a previous list
     // if so turn the + into a - and also put into the state the list
-    setElements([...elements, {text:"testing text"}]);
+    setElements([...elements, 
+      {text:"testing text"}, 
+      {text:"text that is sorta long enough so it just fits"},
+      {text:"text that is super super long so that i can see how the span handles the super long sentences"},
+    ]);
   }
   
   const openBox = () => {
-    if(action == "+") {
-      setAction("-");
+    if(action == "➕") {
+      setAction("—");// "‒ is diff symbol" "— is also different"
       setBox(false);
     } else {
-      setAction("+");
+      setAction("➕");
       setBox(true);
     }
   }
@@ -31,19 +36,19 @@ function List() {
     if(event.key === "Enter") {
       // add new element
       setElements([...elements, {text:event.target.value}]);
+      // remove value in input
+      event.target.value = '';
     }
   }
 
   return (
     <div className="container">
-      <h1>Todo-list<i onClick={openBox} className="add-element">{action}</i></h1>
-      {box && <input type="text" className="addElementBox" onKeyPress={addElement}/>}
-      {/*list elements go here*/}
+      <h1>Todo-list<i onClick={openBox} className="addElementIcon">{action}</i></h1>
+      {/* change box to css so it keeps the text there if u - it */}
+      {box && <input type="text" placeholder="Add a new Todo" className="addElementInput" onKeyPress={addElement}/>}
       {elements.map((object, index) => {
-        return (
-        // <p className="listElements">{object.text}</p>
-        <div></div>
-        );
+        // convert this to context later
+        return (<ListElement key={index} index={index} setElements={setElements} elements={elements} text={object.text}></ListElement>);
       })}
     </div>
   );
--- src/list/listelement.js
+++ src/list/listelement.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './style.css';
 
-function ListElement() {
+function ListElement(props) {
+
+  const [editing, setEditing] = useState(false);
+  const [strikethrough, setStrikethrough] = useState("");
+
+  const deleteElement = () => {
+    var array = [...props.elements]
+    array.splice(props.index, 1);
+    props.setElements(array);
+  }
+  const switchToInput = () => {
+    if(editing) {
+      setEditing(false);
+    } else {
+      setEditing(true);
+    }
+  }
+  const addChanges = (event) => {
+    var array = [...props.elements]
+    array[props.index].text = event.target.value;
+    props.setElements(array);
+  }
+
+  const changeCSS = () => {
+    if(strikethrough === "textStrikethrough") {
+      setStrikethrough("");
+    } else {
+      setStrikethrough("textStrikethrough");
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="listElement">
+      <span onClick={deleteElement} className="deleteElement">D</span>
+      {!editing && <span onClick={changeCSS} className={"textElement" + " " +strikethrough}>{props.text}</span>}
+      {editing && <input type="text" defaultValue={props.text} onChange={addChanges} className="editElementInput"/>}
+      <span onClick={switchToInput} className="editElement">✎</span>
     </div>
   );
 }
